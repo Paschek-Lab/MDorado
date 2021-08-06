ec683a0425ab65db440cec8a19933639c2ab9dea
Jan Neumann <jneumann@carlsberg.pci.uni-rostock.de>
2021-08-06 10:36:29 +0200

    Added file for unittest of hb_analyze

--- mdorado/data/datafilenames.py
+++ mdorado/data/datafilenames.py
@@ -5,4 +5,6 @@ water_trajectory = resource_filename(__name__, 'water.xtc')
 test_gofr_ss = resource_filename(__name__, 'gofr_ss.dat')
 test_gofr_sc = resource_filename(__name__, 'gofr_sc.dat')
 test_gofr_cc = resource_filename(__name__, 'gofr_cc.dat')
+test_hbanalyze = resource_filename(__name__, 'hb_analyze.dat')
+
 del resource_filename
