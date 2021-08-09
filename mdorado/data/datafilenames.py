3c4c3ca1cc6eb6476d63df5661e37221516af4c4
neujan <jneumann@carlsberg.pci.uni-rostock.de>
2021-08-09 09:56:20 +0200

    Added testfiles for lifetime unittest

--- mdorado/data/datafilenames.py
+++ mdorado/data/datafilenames.py
@@ -6,5 +6,6 @@ test_gofr_ss = resource_filename(__name__, 'gofr_ss.dat')
 test_gofr_sc = resource_filename(__name__, 'gofr_sc.dat')
 test_gofr_cc = resource_filename(__name__, 'gofr_cc.dat')
 test_hbanalyze = resource_filename(__name__, 'hb_analyze.dat')
+test_lifetime = resource_filename(__name__, 'lifetime_test.dat')
 
 del resource_filename
