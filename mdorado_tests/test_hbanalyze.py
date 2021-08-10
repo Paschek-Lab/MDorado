137f8b10580fd4f6eda63330ba713359c1628c91
neujan <88310018+neujan@users.noreply.github.com>
2021-08-10 08:42:12 +0200

    Updated file management of unit tests

--- mdorado_tests/test_hbanalyze.py
+++ mdorado_tests/test_hbanalyze.py
@@ -1,4 +1,5 @@
 import unittest
+import tempfile
 import os
 import numpy as np
 import MDAnalysis
@@ -10,12 +11,13 @@ class TestProgram(unittest.TestCase):
         u = MDAnalysis.Universe(water_topology, water_trajectory)
         xgrp = u.select_atoms("name ow")
         hgrp = u.select_atoms("name hw")[::2]
-        hb_analyze(universe=u, xgrp=xgrp, hgrp=hgrp, rmin=1.5, rmax=5, cosalphamin=-1, cosalphamax=1, bins=50, outfilename="test.dat")
-        matrix = np.loadtxt("test.dat")
+        with tempfile.TemporaryDirectory() as tmpdirname:
+            os.chdir(tmpdirname)
+            hb_analyze(universe=u, xgrp=xgrp, hgrp=hgrp, rmin=1.5, rmax=5, cosalphamin=-1, cosalphamax=1, bins=50, outfilename="test.dat")
+            matrix = np.loadtxt("test.dat")
 
-        test_matrix = np.loadtxt(test_hbanalyze)
-        self.assertIsNone(np.testing.assert_array_almost_equal(matrix, test_matrix))
-        os.remove("test.dat")
+            test_matrix = np.loadtxt(test_hbanalyze)
+            self.assertIsNone(np.testing.assert_array_almost_equal(matrix, test_matrix))
 
 if __name__ == '__main__':
         unittest.main()
