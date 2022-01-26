41d534b5b329561fbf75ead79fda583104750eb4
neujan <jneumann@schumacher.pci.uni-rostock.de>
2022-01-26 15:53:57 +0100

    Started new Version 0.3.0 with changes to the veccor module and creation of a vectors and dipol relaxation module

--- mdorado/__init__.py
+++ mdorado/__init__.py
@@ -1,13 +1,15 @@
 #Version information. Keep up-to-date with version in setup.py
 
-__version__ = "0.2.1"
+__version__ = "0.3.0"
 
 #list of all modules
 __modulelist__ = [
             "correlations",
             "gofr",
+            "vectors"
             "hb_analyze", 
             "lifetime",
             "msd",
             "veccor"
+            "dipol_relax"
             ]
